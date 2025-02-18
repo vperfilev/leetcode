e48f9e7a0a2135f7220adaa19e0cbae37e993105
Vlad Perfilev <perfilev.vlad@yandex.ru>
2025-02-18 10:19:28 +0300

    Add 2 tasks

---
+++ DynamicProgramming/63. Unique Paths II.cs
@@ -0,0 +1,81 @@
+﻿namespace DynamicProgramming;
+
+public sealed class UniquePaths2 
+{
+    public class Solution
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            var m = obstacleGrid.Length;
+            var n = obstacleGrid[0].Length;
+            
+            var dp = new int[m, n];
+
+
+            var faceObstacle = false;
+            for (var i = 0; i < m; i++)
+            {
+                if (obstacleGrid[i][0] == 1) faceObstacle = true;
+                dp[i, 0] = faceObstacle ? int.MaxValue : 1;
+            }
+
+            faceObstacle = false;
+            for (var i = 0; i < n; i++)
+            {
+                if (obstacleGrid[0][i] == 1) faceObstacle = true;
+                dp[0, i] = faceObstacle ? int.MaxValue : 1;
+            }
+
+            for (var i = 1; i < m; i++)
+            {
+                for (var j = 1; j < n; j++)
+                {
+                    if (obstacleGrid[i][j] == 1)
+                    {
+                        dp[i, j] = int.MaxValue;
+                    }
+                    else
+                    {
+                        dp[i, j] = (dp[i, j - 1], dp[i - 1, j]) switch
+                        {
+                            (int.MaxValue, int.MaxValue) => int.MaxValue,
+                            (var a, int.MaxValue) => a,
+                            (int.MaxValue, var b) => b,
+                            var (a, b) => a + b
+                        };
+                    }
+                }
+            }
+
+            return dp[m - 1, n - 1] == int.MaxValue ? 0 : dp[m - 1, n - 1];
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[][] obstacleGrid = [[0, 0, 0], [0, 1, 0], [0, 0, 0]];
+        
+        // Act
+        var pathCount = sut.UniquePathsWithObstacles(obstacleGrid);
+        
+        // Assert
+        Assert.Equal(2, pathCount);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[][] obstacleGrid = [[0, 1], [0, 0]];
+        
+        // Act
+        var pathCount = sut.UniquePathsWithObstacles(obstacleGrid);
+        
+        // Assert
+        Assert.Equal(1, pathCount);
+    }
+}
---
+++ DynamicProgramming/64. Minimum Path Sum.cs
@@ -0,0 +1,58 @@
+﻿namespace DynamicProgramming;
+
+public sealed class MinimumPathSum {
+    public class Solution
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            var m = grid.Length;
+            var n = grid[0].Length;
+            var dp = new int[m, n];
+            dp[0, 0] = grid[0][0];
+
+            for (var i = 1; i < m; i++)
+                dp[i, 0] = dp[i - 1, 0] + grid[i][0];
+
+            for (var i = 1; i < n; i++)
+                dp[0, i] = dp[0, i - 1] + grid[0][i];
+
+            for (var i = 1; i < m; i++)
+            {
+                for (var j = 1; j < n; j++)
+                {
+                    dp[i, j] = grid[i][j] + Math.Min(dp[i - 1, j], dp[i, j - 1]);
+                }
+            }
+
+            return dp[m - 1, n - 1];
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[][] grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]];
+        
+        // Act
+        var pathCost = sut.MinPathSum(grid);
+        
+        // Assert
+        Assert.Equal(7, pathCost);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[][] grid = [[1, 2, 3], [4, 5, 6]];
+        
+        // Act
+        var pathCost = sut.MinPathSum(grid);
+        
+        // Assert
+        Assert.Equal(12, pathCost);
+    }
+}
