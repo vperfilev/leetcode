2f7541cc8f0d74525cfb16e87961cd43f6f120e4
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-05-06 14:40:41 +0300

    Add two tasks

---
+++ TopInterview150/205. Isomorphic Strings.cs
@@ -0,0 +1,91 @@
+﻿namespace TopInterview150;
+
+public sealed class IsomorphicStrings
+{
+    public class Solution
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            Span<char> sToT = stackalloc char[256];
+            Span<char> tToS = stackalloc char[256];
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var sChar = s[i];
+                var tChar = t[i];
+                
+                if (sToT[sChar] == 0)
+                    sToT[sChar] = tChar;
+                else if (sToT[sChar] != tChar)
+                    return false;
+                
+                if (tToS[tChar] == 0)
+                    tToS[tChar] = sChar;
+                else if (tToS[tChar] != sChar)
+                    return false;
+            }
+            return true;
+        }
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "egg";
+        var t = "add";
+        
+        // Act
+        var isIsomorphic = sut.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.True(isIsomorphic);
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "foo";
+        var t = "bar";
+        
+        // Act
+        var isIsomorphic = sut.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.False(isIsomorphic);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "paper";
+        var t = "title";
+        
+        // Act
+        var isIsomorphic = sut.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.True(isIsomorphic);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "badc";
+        var t = "baba";
+        
+        // Act
+        var isIsomorphic = sut.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.False(isIsomorphic);
+    }
+
+}
---
+++ TopInterview150/290. Word Pattern.cs
@@ -0,0 +1,102 @@
+﻿namespace TopInterview150;
+
+public class WordPattern
+{
+    public class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            var words = s.Split(' ');
+            
+            if (words.Length != pattern.Length)
+                return false;
+            
+            var charToWord = new Dictionary<char, string>();
+            var wordToChar = new Dictionary<string, char>();
+            
+            for (var i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+                var patternSymbol = pattern[i];
+
+                if (charToWord.TryGetValue(patternSymbol, out var w))
+                {
+                    if (word != w) return false;
+                }
+                else
+                {
+                    if (wordToChar.TryGetValue(word, out var c) && c != patternSymbol)
+                        return false;
+                    
+                    wordToChar[word] = patternSymbol;
+                    charToWord[patternSymbol] = word;
+                }
+            }
+            return true;
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var solution = new Solution();
+     
+        var pattern = "abba";
+        var s = "dog cat cat dog";
+        
+        // Act
+        var matched = solution.WordPattern(pattern, s);
+        
+        // Assert
+        Assert.True(matched);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var solution = new Solution();
+        
+        var pattern = "abba";
+        var s = "dog cat cat fish";
+        
+        // Act
+        var matched = solution.WordPattern(pattern, s);
+        
+        // Assert
+        Assert.False(matched);
+    }
+    
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var solution = new Solution();
+        
+        var pattern = "aaaa";
+        var s = "dog cat cat dog";
+        
+        // Act
+        var matched = solution.WordPattern(pattern, s);
+        
+        // Assert
+        Assert.False(matched);
+    }
+    
+    [Fact]
+    public void Test4()
+    {
+        // Arrange
+        var solution = new Solution();
+        
+        var pattern = "abba";
+        var s = "dog dog dog dog";
+        
+        // Act
+        var matched = solution.WordPattern(pattern, s);
+        
+        // Assert
+        Assert.False(matched);
+    }
+}
