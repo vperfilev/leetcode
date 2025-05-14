fb5698f1f747917837c4e910aa8acc057a1b84c9
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-05-14 16:44:38 +0300

    Add two tasks

---
+++ TopInterview150/242. Valid Anagram.cs
@@ -0,0 +1,52 @@
+﻿namespace TopInterview150;
+
+public sealed class ValidAnagram
+{
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            
+            var letterCount = new int[26];
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                letterCount[s[i] - 'a']++;
+                letterCount[t[i] - 'a']--;
+            }
+
+            return letterCount.All(x => x == 0);
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "anagram";
+        var t = "nagaram";
+
+        // Act
+        var isAnagram = sut.IsAnagram(s, t);
+
+        // Assert
+        Assert.True(isAnagram);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        var s = "rat";
+        var t = "car";
+
+        // Act
+        var isAnagram = sut.IsAnagram(s, t);
+
+        // Assert
+        Assert.False(isAnagram);
+    }
+}
---
+++ TopInterview150/49. Group Anagrams.cs
@@ -0,0 +1,66 @@
+﻿namespace TopInterview150;
+
+public sealed class GroupAnagrams
+{
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            return strs.GroupBy(GetFreqKey)
+                .Select(x => (IList<string>)x.ToList())
+                .ToList();
+        }
+
+        private string GetFreqKey(string str)
+        {
+            var letterCount = new int[26];
+
+            foreach (var c in str)
+                letterCount[c - 'a']++;
+
+            return string.Join(",", letterCount);
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        string[] strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+
+        // Act
+        var grouped = sut.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal([["eat", "tea", "ate"], ["tan", "nat"], ["bat"]], grouped);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        string[] strs = [""];
+
+        // Act
+        var groupped = sut.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal(groupped, [[""]]);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var sut = new Solution();
+        string[] strs = ["a"];
+
+        // Act
+        var groupped = sut.GroupAnagrams(strs);
+
+        // Assert
+        Assert.Equal(groupped, [["a"]]);
+    }
+}
