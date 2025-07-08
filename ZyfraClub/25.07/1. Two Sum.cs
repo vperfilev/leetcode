e46c4ce40b8c365a6a06b1818f7295d694ae6e91
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-07-08 10:40:42 +0300

    Add tasks

---
+++ ZyfraClub/25.07/1. Two Sum.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+
+namespace ZyfraClub._25._07;
+
+public class TwoSum 
+{
+    public class Solution 
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var compliments = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var compliment = target - nums[i];
+                if (compliments.TryGetValue(compliment, out var index))
+                {
+                    return [index, i];
+                }
+
+                compliments[nums[i]] = i;
+            }
+
+            return [];
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[] nums = [2,7,11,15];
+        var target = 9;
+        
+        // Act
+        var indexes = sut.TwoSum(nums, target);
+        
+        // Assert
+        Assert.Equal([0,1], indexes);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[] nums = [3,2,4];
+        var target = 6;
+        
+        // Act
+        var indexes = sut.TwoSum(nums, target);
+        
+        // Assert
+        Assert.Equal([1,2], indexes);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var sut = new Solution();
+        int[] nums = [3,3];
+        var target = 6;
+        
+        // Act
+        var indexes = sut.TwoSum(nums, target);
+        
+        // Assert
+        Assert.Equal([0,1], indexes);
+    }
+}
---
+++ ZyfraClub/25.07/404. Sum of Left Leaves.cs
@@ -0,0 +1,77 @@
+﻿using Xunit;
+
+namespace ZyfraClub._25._07;
+
+public sealed class SumOfLeftLeaves 
+{
+    public class Solution 
+    {
+        public int SumOfLeftLeaves(TreeNode root) 
+        {
+            var nodes = new Queue<TreeNode>();
+            nodes.Enqueue(root);
+            
+            var leftSum = 0;
+            
+            while(nodes.TryDequeue(out var node))
+            {
+                Enqueue(node.left);
+                Enqueue(node.right);
+            
+                if (IsLeaf(node.left))
+                    leftSum += node.left!.val;
+            }
+            return leftSum;
+
+            void Enqueue(TreeNode? node){
+                if (node != null && !IsLeaf(node))
+                    nodes.Enqueue(node);
+            };
+            bool IsLeaf(TreeNode? node) => node is { left: null, right: null };
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode? left;
+            public TreeNode? right;
+
+            public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var sut = new Solution();
+            var root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+            
+            // Act
+            var sum = sut.SumOfLeftLeaves(root);
+            
+            // Assert
+            Assert.Equal(24, sum);
+        }
+        
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var sut = new Solution();
+            var root = new TreeNode(1);
+            
+            // Act
+            var sum = sut.SumOfLeftLeaves(root);
+            
+            // Assert
+            Assert.Equal(0, sum);
+        }
+    }    
+}
