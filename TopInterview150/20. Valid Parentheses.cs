9d18b58803d86215f479a8c3f97b85744c982cad
Vlad Perfilev <perfilev.vlad@yandex.ru>
2025-07-09 09:21:39 +0300

    Add task

---
+++ TopInterview150/20. Valid Parentheses.cs
@@ -0,0 +1,53 @@
+﻿namespace TopInterview150;
+
+public sealed class ValidParentheses 
+{
+    public class Solution 
+    {
+        private readonly Dictionary<char, char> _symbolPairs = new()
+        {
+            [')'] = '(',
+            [']'] = '[',
+            ['}'] = '{'
+        };
+
+        public bool IsValid(string s) 
+        {
+            var symbolStack = new Stack<char>();
+
+            foreach(var symbol in s)
+            {
+                if(_symbolPairs.TryGetValue(symbol, out var openSymbol))
+                {
+                    if (symbolStack.Count == 0 || symbolStack.Peek() != openSymbol)
+                        return false;
+
+                    _ = symbolStack.Pop();
+                }
+                else
+                {
+                    symbolStack.Push(symbol);
+                }
+            }
+
+            return symbolStack.Count == 0;
+        }
+    }
+
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    [InlineData("([])", true)]
+    public void Test(string s, bool expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        
+        // Act
+        var isValid = sut.IsValid(s);
+        
+        // Assert
+        Assert.Equal(expected, isValid);
+    }
+}
