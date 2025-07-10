2c1f35398f45e5cc9c83a86b497eb10c9e437522
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-07-10 10:40:22 +0300

    Add task

---
+++ TopInterview150/71. Simplify Path.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TopInterview150;
+
+public sealed class SimplifyPath
+{
+    public class Solution
+    {
+        public string SimplifyPath(string path)
+        {
+            var segments = new List<string>();
+
+            foreach (var segment in Regex.Split(path, "/+").Where(s => !string.IsNullOrEmpty(s)))
+                switch (segment)
+                {
+                    case "..":
+                    {
+                        if (segments.Count > 0)
+                            segments.RemoveAt(segments.Count - 1);
+                        break;
+                    }
+                    case ".":
+                        break;
+                    default:
+                        segments.Add(segment);
+                        break;
+                }
+
+            return $"/{string.Join('/', segments)}";
+        }
+    }
+
+    [Theory]
+    [InlineData("/home/", "/home")]
+    [InlineData("/home//foo/", "/home/foo")]
+    [InlineData("/home/user/Documents/../Pictures", "/home/user/Pictures")]
+    [InlineData("/../", "/")]
+    [InlineData("/.../a/../b/c/../d/./", "/.../b/d")]
+    public void Test(string path, string expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        
+        // Act
+        var simplifiedPath = sut.SimplifyPath(path);
+        
+        // Assert
+        Assert.Equal(expected, simplifiedPath);
+    }
+}
