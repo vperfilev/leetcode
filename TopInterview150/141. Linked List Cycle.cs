1d1f2facc090027d1176d5853077f244e587146c
Vlad Perfilev <perfilev.vlad@yandex.ru>
2025-07-12 19:28:50 +0300

    Add task

---
+++ TopInterview150/141. Linked List Cycle.cs
@@ -0,0 +1,90 @@
+﻿namespace TopInterview150;
+
+public sealed class LinkedListCycle
+{
+    public class Solution
+    {
+        public bool HasCycle(ListNode? head)
+        {
+            var current = head;
+
+            var dict = new HashSet<ListNode>();
+            while (current is not null)
+            {
+                if (dict.Contains(current))
+                    return true;
+
+                dict.Add(current);
+                current = current.next;
+            }
+
+            return false;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var sut = new Solution();
+        var head = CreateLinkedList([3, 2, 0, -4], 1);
+
+        // Act
+        var hasCycle = sut.HasCycle(head);
+
+        // Assert
+        Assert.True(hasCycle);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var sut = new Solution();
+        var head = CreateLinkedList([1, 2], 0);
+
+        // Act
+        var hasCycle = sut.HasCycle(head);
+
+        // Assert
+        Assert.True(hasCycle);
+    }
+
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var sut = new Solution();
+        var head = CreateLinkedList([1], -1);
+
+        // Act
+        var hasCycle = sut.HasCycle(head);
+
+        // Assert
+        Assert.False(hasCycle);
+    }
+
+    private ListNode? CreateLinkedList(int[] ints, int pos)
+    {
+        var nodes = ints.Select(x => new ListNode(x)).ToArray();
+        for (var i = 0; i < ints.Length - 1; i++)
+            nodes[i].next = nodes[i + 1];
+
+        if (pos >= 0)
+            nodes[^1].next = nodes[pos];
+
+        return nodes.FirstOrDefault();
+    }
+}
---
+++ TopInterview150/21. Merge Two Sorted Lists.cs
@@ -0,0 +1,102 @@
+﻿namespace TopInterview150;
+
+public sealed class MergeTwoSortedLists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+        {
+            if (list1 == null || list2 == null)
+                return list1 ?? list2;
+
+            var head = SelectMinValue(ref list1, ref list2);
+            var prev = head;
+
+            while (list1 != null && list2 != null)
+            {
+                var nextNode = SelectMinValue(ref list1, ref list2);
+                prev!.next = nextNode;
+                prev = nextNode;
+            }
+
+            prev!.next = list1 ?? list2;
+
+            return head;
+
+            static ListNode? SelectMinValue(ref ListNode? list1, ref ListNode? list2)
+            {
+                ListNode? result;
+                if (list1!.val < list2!.val)
+                {
+                    result = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    result = list2;
+                    list2 = list2.next;
+                }
+
+                return result;
+            }
+        }
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 4 }, new[] { 1, 3, 4 }, new[] { 1, 1, 2, 3, 4, 4 })]
+    [InlineData(new int[] { }, new int[] { }, new int[] { })]
+    [InlineData(new int[] { }, new[] { 0 }, new[] { 0 })]
+    public void Test(int[] list1, int[] list2, int[] expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        var l1 = CreateLinkedList(list1);
+        var l2 = CreateLinkedList(list2);
+        
+        // Act
+        var merged = sut.MergeTwoLists(l1, l2);
+        
+        // Assert
+        Assert.Equal(expected, ToArray(merged));
+    }
+
+    private int[] ToArray(ListNode? listNode)
+    {
+        var result = new List<int>();
+        while (listNode != null)
+        {
+            result.Add(listNode.val);
+            listNode = listNode.next;
+        }
+        return result.ToArray();
+    }
+
+    private ListNode? CreateLinkedList(int[] list1)
+    {
+        if (list1.Length == 0)
+            return null;
+
+        var head = new ListNode(list1[0]);
+        var current = head;
+
+        for (var i = 1; i < list1.Length; i++)
+        {
+            current.next = new ListNode(list1[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+}
