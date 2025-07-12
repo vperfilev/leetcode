13e83bb3958daaec2ecf61e8cda78efb23735015
Vlad Perfilev <perfilev.vlad@yandex.ru>
2025-07-13 02:30:19 +0300

    Add tasks

---
+++ TopInterview150/138. Copy List with Random Pointer.cs
@@ -0,0 +1,125 @@
+﻿namespace TopInterview150;
+
+public sealed class CopyListWithRandomPointer
+{
+    public class Node
+    {
+        public int val;
+        public Node? next;
+        public Node? random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
+
+    public class Solution
+    {
+        public Node? CopyRandomList(Node head)
+        {
+            if (head is null)
+                return head;
+            
+            var current = head;
+
+            while (current != null)
+            {
+                var copy = new Node(current.val)
+                {
+                    next = current.next,
+                    random = current.random
+                };
+                current.next = copy;
+                current = copy.next;
+            }
+
+            current = head.next;
+            var newHead = head.next;
+            while (current != null)
+            {
+                current.random = current.random?.next;
+                current = current.next?.next;
+            }
+            
+            current = head;
+            while (current != null)
+            {
+                var nextNode = current.next;
+                current.next = current.next?.next;
+                current = nextNode;
+            }
+
+            return newHead;
+        }
+    }
+
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[] { new int?[][] { new int?[] { 7, null }, new int?[] { 13, 0 }, new int?[] { 11, 4 }, new int?[] { 10, 2 }, new int?[] { 1, 0 } } },
+            new object[] { new int?[][] { new int?[] { 1, 1 }, new int?[] { 2, 1 } } },
+            new object[] { new int?[][] { new int?[] { 3, null }, new int?[] { 3, 0 }, new int?[] { 3, null } } }
+        };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(int?[][] input)
+    {
+        // Arrange
+        var solution = new Solution();
+        var head = BuildList(input);
+
+        // Act
+        var copy = solution.CopyRandomList(head);
+
+        // Assert
+        var expected = BuildList(input);
+        AssertListsEqual(expected, copy);
+    }
+    
+    private Node? BuildList(int?[][] nodes)
+    {
+        if (nodes.Length == 0) return null;
+        Node?[] nodeArr = new Node[nodes.Length];
+        for (int i = 0; i < nodes.Length; i++)
+            nodeArr[i] = new Node(nodes[i][0].Value);
+
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            if (i < nodes.Length - 1)
+                nodeArr[i].next = nodeArr[i + 1];
+            int? randomIndex = nodes[i][1];
+            if (randomIndex.HasValue)
+                nodeArr[i].random = nodeArr[randomIndex.Value];
+        }
+        return nodeArr[0];
+    }
+
+    // Вспомогательный метод для проверки эквивалентности списков
+    private void AssertListsEqual(Node? expected, Node? actual)
+    {
+        var expectedNodes = new List<Node?>();
+        var actualNodes = new List<Node?>();
+        var e = expected;
+        var a = actual;
+        while (e != null && a != null)
+        {
+            expectedNodes.Add(e);
+            actualNodes.Add(a);
+            e = e.next;
+            a = a.next;
+        }
+        Assert.True(e == null && a == null, "Lists have different lengths");
+
+        for (int i = 0; i < expectedNodes.Count; i++)
+        {
+            Assert.Equal(expectedNodes[i].val, actualNodes[i].val);
+            int? expectedRandom = expectedNodes[i].random == null ? (int?)null : expectedNodes.IndexOf(expectedNodes[i].random);
+            int? actualRandom = actualNodes[i].random == null ? (int?)null : actualNodes.IndexOf(actualNodes[i].random);
+            Assert.Equal(expectedRandom, actualRandom);
+        }
+    }
+}
---
+++ TopInterview150/2. Add Two Numbers.cs
@@ -0,0 +1,84 @@
+﻿namespace TopInterview150;
+
+public sealed class AddTwoNumbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode? AddTwoNumbers(ListNode? l1, ListNode? l2)
+        {
+            ListNode? result = null;
+            ListNode? prev = null;
+            var caryOn = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                var sum = (l1?.val ?? 0) + (l2?.val ?? 0) + caryOn;
+                caryOn = sum / 10;
+                sum -= caryOn * 10;
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+
+                var node = new ListNode(sum);
+                if (prev != null)
+                    prev.next = node;
+                prev = node;
+                result ??= node;
+            }
+
+            if (caryOn > 0)
+                prev!.next = new ListNode(caryOn);
+
+            return result;
+        }
+    }
+    
+    [Theory]
+    [InlineData(new[] { 2, 4, 3 }, new[] { 5, 6, 4 }, new[] { 7, 0, 8 })]
+    [InlineData(new[] { 0 }, new[] { 0 }, new[] { 0 })]
+    [InlineData(new[] { 9, 9, 9, 9, 9, 9, 9 }, new[] { 9, 9, 9, 9 }, new[] { 8, 9, 9, 9, 0, 0, 0, 1 })]
+    public void TestAddTwoNumbers(int[] l1, int[] l2, int[] expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        var list1 = CreateLinkedList(l1);
+        var list2 = CreateLinkedList(l2);
+
+        // Act
+        var result = sut.AddTwoNumbers(list1, list2);
+
+        // Assert
+        Assert.Equal(expected, ConvertToArray(result));
+    }
+
+    private int[] ConvertToArray(ListNode nodes)
+    {
+        var result = new List<int>();
+        while (nodes != null)
+        {
+            result.Add(nodes.val);
+            nodes = nodes.next;
+        }
+        return result.ToArray();
+    }
+
+    private ListNode? CreateLinkedList(int[] values)
+    {
+        var nodes = values.Select(x => new ListNode(x)).ToArray();
+        for (var i = 0; i < values.Length - 1; i++)
+            nodes[i].next = nodes[i + 1];
+        return nodes.FirstOrDefault();
+    }
+}
