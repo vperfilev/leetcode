fab5c91d4df60d51824bc4ce76af47b5e76aa627
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-08-06 16:28:11 +0300

    Add tasks

---
+++ ZyfraClub/25.08/2210. Count Hills and Valleys in an Array.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace ZyfraClub._25._08;
+
+public sealed class CountHillsAndValleysInAnArray 
+{
+    public class Solution 
+    {
+        public int CountHillValley(int[] nums) 
+        {
+            var count = 0;
+            var prevSign = 0;
+
+            for(var i = 1; i < nums.Length; i++)
+            {
+                var change = nums[i] - nums[i - 1];
+                if (change == 0) continue;
+
+                var sign = Math.Sign(change);
+                if (prevSign != 0 && prevSign != sign) count++;
+                prevSign = sign;
+            }
+            return count;    
+        }
+    }
+
+    [Theory]
+    [InlineData(new[] {2,4,1,1,6,5}, 3)]
+    [InlineData(new[] {6,6,5,5,4,1}, 0)]
+    public void Test(int[] nums, int expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        
+        // Act
+        var countHillValley = sut.CountHillValley(nums);
+        
+        // Assert
+        Assert.Equal(expected, countHillValley);
+    }
+}
---
+++ ZyfraClub/25.08/2859. Sum of Values at Indices With K Set Bits.cs
@@ -0,0 +1,43 @@
+﻿using Xunit;
+
+namespace ZyfraClub._25._08;
+
+public sealed class SumOfValuesAtIndicesWithKSetBits 
+{
+    public class Solution 
+    {
+        public int SumIndicesWithKSetBits(IList<int> nums, int k) 
+        {
+            var sum = 0;
+            for(var i = 0; i < nums.Count; i++)
+                if (CountBits(i) == k)
+                    sum += nums[i];
+            return sum;
+        }
+
+        private int CountBits(int n)
+        {
+            var count = 0;
+            while (n > 0){
+                count += n & 1;
+                n >>= 1;
+            }
+            return count;
+        }
+    }
+
+    [Theory]
+    [InlineData(new []{5,10,1,5,2}, 1, 13)]
+    [InlineData(new []{4,3,2,1}, 2, 1)]
+    public void MethodName(int[] nums, int k, int expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        
+        // Act
+        var sumIndicesWithKSetBits = sut.SumIndicesWithKSetBits(nums, k);
+        
+        // Assert
+        Assert.Equal(expected, sumIndicesWithKSetBits);
+    }
+}
