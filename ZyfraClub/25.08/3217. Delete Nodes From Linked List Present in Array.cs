8b9614432b6796988167d1146010c2ed2c1143dc
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-08-08 13:13:53 +0300

    Add task

---
+++ ZyfraClub/25.08/3217. Delete Nodes From Linked List Present in Array.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using ZyfraClub.Common;
+
+namespace ZyfraClub._25._08;
+
+public sealed class DeleteNodesFromLinkedListPresentInArray
+{
+    public class Solution
+    {
+        public ListNode ModifiedList(int[] nums, ListNode head)
+        {
+            var dummy = new ListNode(0, head);
+            var valuesToRemove = new HashSet<int>(nums);
+            head = dummy;
+            while (head.next != null)
+                if (valuesToRemove.Contains(head.next.val))
+                    head.next = head.next.next;
+                else
+                    head = head.next;
+
+            return dummy.next;
+        }
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 2, 3 }, new[] { 1, 2, 3, 4, 5 }, new[] { 4, 5 })]
+    [InlineData(new[] { 1 }, new[] { 1, 2, 1, 2, 1, 2 }, new[] { 2, 2, 2 })]
+    [InlineData(new[] { 5 }, new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 })]
+    public void Test(int[] nums, int[] headValues, int[] expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        var head = ListNode.FromValues(headValues);
+
+        // Act
+        var modifiedList = sut.ModifiedList(nums, head);
+
+        // Assert
+        Assert.Equal(modifiedList.Values, expected);
+    }
+}
---
+++ ZyfraClub/Common/ListNode.cs
@@ -0,0 +1,43 @@
+﻿namespace ZyfraClub.Common;
+
+public sealed class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+    public IEnumerable<int> Values
+    {
+        get
+        {
+            var head = this;
+            var values = new List<int>();
+            while (head != null)
+            {
+                values.Add(head.val);
+                head = head.next;
+            }
+
+            return values;
+        }
+    }
+
+    public static ListNode FromValues(int[] headValues)
+    {
+        var head = new ListNode(headValues[0]);
+        var current = head;
+
+        for (var i = 1; i < headValues.Length; i++)
+        {
+            current.next = new ListNode(headValues[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+}
