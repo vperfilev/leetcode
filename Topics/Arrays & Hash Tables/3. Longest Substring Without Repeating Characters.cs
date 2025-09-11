6a2f6592c81b9df3b3e048ed8104daee02311c06
vladislav.perfilev <perfilev.vlad@yandex.ru>
2025-09-11 17:58:12 +0300

    Add new project and tasks

---
+++ Topics/Arrays & Hash Tables/3. Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Topics.Arrays___Hash_Tables;
+
+public sealed class LongestSubstringWithoutRepeatingCharacters 
+{
+    public class Solution 
+    {
+        public int LengthOfLongestSubstring(string s) 
+        {
+            var maxLength = 0;
+            var symbols = new HashSet<char>();
+            for(int left = 0, right = 0; right < s.Length; right++)
+            {
+                while(!symbols.Add(s[right]))
+                {
+                    symbols.Remove(s[left++]);
+                }
+                maxLength = Math.Max(maxLength, symbols.Count);
+            } 
+            return maxLength;   
+        }
+    }
+    
+    [Theory]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("bbbbb", 1)]
+    [InlineData("pwwkew", 3)]
+    public void Test(string s, int expected)
+    {
+        // Arrange
+        var sut = new Solution();
+        
+        // Act
+        var lengthOfLongestSubstring = sut.LengthOfLongestSubstring(s);
+        
+        // Assert
+        Assert.Equal(expected, lengthOfLongestSubstring);
+    }
+}
---
+++ Topics/Arrays & Hash Tables/560. Subarray Sum Equals K.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace Topics.Arrays___Hash_Tables;
+
+public sealed class SubarraySumEqualsK
+{
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            var currentSum = 0;
+            var result = 0;
+            var prefixSumCount = new Dictionary<int, int> { { 0, 1 } };
+            foreach (var num in nums)
+            {
+                currentSum += num;
+                if (prefixSumCount.ContainsKey(currentSum - k)) result += prefixSumCount[currentSum - k];
+
+                prefixSumCount[currentSum] = prefixSumCount.GetValueOrDefault(currentSum, 0) + 1;
+            }
+
+            return result;
+        }
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 1, 1 }, 2, 2)]
+    [InlineData(new[] { 1, 2, 3 }, 3, 2)]
+    public void Test(int[] nums, int k, int expected)
+    {
+        // Arrange
+        var sut = new Solution();
+
+        // Act
+        var subarraySum = sut.SubarraySum(nums, k);
+
+        // Assert
+        Assert.Equal(expected, subarraySum);
+    }
+}
